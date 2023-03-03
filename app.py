aefc859c7ea90b44f74272edfda2db99fe7e088e
Zach Zaro <zach@withcoherence.com>
2023-03-03 21:10:08 +0000

    port change

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def generate():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=True)
 
