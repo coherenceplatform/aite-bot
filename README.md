7c4370a664554453cd5e5fa3211c62cc9e6f1cc5
Zach Zaro <zach@withcoherence.com>
2023-03-09 00:44:36 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Running on Coherence
+
+- Use the `Use this Template` menu in the top right to create your own copy of this repo.
+- Set up the app in AWS/GCP using [Coherence](https://www.withcoherence.com) by installing the github app in your repo, and linking your cloud account on GCP or AWS, by following the Coherence [setup wizard](https://app.withcoherence.com).
+- Set the `OPENAI_API_KEY` variable in your app using the Coherence UI, set it to the value from your `OpenAI` account.
+- Visit the URL on your initial Coherence environment!
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, session, redirect
+import os
+
+from ai_logic import (
+    generate_plan,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    uploaded_file = request.files["file"]
+    plan = generate_plan(uploaded_file.read())
+    return render_template("index.html", test_plan=plan)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
